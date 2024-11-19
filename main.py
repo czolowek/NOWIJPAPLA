1e80001070e438d6fab0032352d182724fa27cb3
czolowek <klutoi39@gmail.com>
2024-11-19 12:38:24 +0100

    vxdcgbc

--- main.py
+++ main.py
@@ -1,22 +1,36 @@
-from flask import Flask,render_template
+from flask import Flask, render_template, request, redirect
+
 import db
+
 
 app = Flask(__name__)
 
 
-@app.get("/")
-def index():
-    return render_template("index.html")
-
-
 @app.get("/menu/")
 def menu():
-    students = [
-        {"name": "марко", "last_name":"аирович", "rost": 166},
-        {"name": "игорь", "last_name": "амбасадор", "rost":177 },
-        {"name": "макс", "last_name": "аркадий", "rost":107 }
-   ] 
-    return render_template("menu.html",students=students)
+    pizzas_db = db.get_pizzas()
+    pizzas = []
+
+    for pizza in pizzas_db:
+        pizzas.append(
+            {"name": pizza[1], "ingredients": pizza[2], "price": pizza[3]}
+        )
+
+    return render_template("menu.html", pizzas=pizzas)
+
+
+@app.route("/add_pizza/", methods=["GET", "POST"])
+def add_pizza():
+    if request.method == "POST":
+        name = request.form.get("name")
+        ingredients = request.form.get("ingredients")
+        price = request.form.get("price")
+
+        db.add_pizza(name, ingredients, price)
+        return redirect("/menu/")
+
+    return render_template("add_pizza.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=80)
